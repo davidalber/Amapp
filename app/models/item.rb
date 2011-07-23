afa8db46e751e5a942e39dc2be83f31b4e48806e
David Alber <alber.david@gmail.com>
2011-07-22 18:03:29 -0700

    - Moved the code to do item lookup into its own method to avoid   possible future duplication. - Removed the after validation step that was setting the title   following an item lookup. Setting the title is now done during   validation.

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,34 +8,29 @@ class Item < ActiveRecord::Base
   validates_uniqueness_of :asin
   validate :is_valid_asin
 
-  after_validation :set_title
-
 private
-  def set_title
-    il = ItemLookup.new( 'ASIN', { 'ItemId' => asin,
+  # Make an item lookup request using Amazon's API with the ASIN. Exceoptions
+  # are not caught in this method.
+  def get_item(rg)
+    il = ItemLookup.new( 'ASIN', { 'ItemId' => self.asin,
                            'MerchantId' => 'Amazon' } )
+    il.response_group = rg
     req = Search::Request.new()
 
+    return req.search(il)
+  end
+
+  # Check if the ASIN is a valid Amazon item and set title if it is.
+  def is_valid_asin
+    rg = ResponseGroup.new(:Small)
     begin
-      resp = req.search(il)
+      resp = get_item(rg)
       item = resp.item_lookup_response[0].items[0].item
       self.title = item.item_attributes.title.to_s
     rescue Error::InvalidParameterValue
       errors.add(:asin, "Invalid ASIN")
+      return false
     end
-  end
-
-  def is_valid_asin
-    il = ItemLookup.new( 'ASIN', { 'ItemId' => self.asin,
-                           'MerchantId' => 'Amazon' } )
-    il.response_group = :Small
-
-    req = Search::Request.new()
-
-    begin
-      resp = req.search(il)
-    rescue Error::InvalidParameterValue
-      errors.add(:asin, "Invalid ASIN")
-    end
+    return true
   end
 end
