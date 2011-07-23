2f31673b8bfcf075dcc5fce269de9bacfcd7a6ec
David Alber <alber.david@gmail.com>
2011-07-22 17:43:38 -0700

    Sort list of items in the item index view.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.xml
   def index
-    @items = Item.all
+    @items = Item.all.sort_by &:title
 
     respond_to do |format|
       format.html # index.html.erb
